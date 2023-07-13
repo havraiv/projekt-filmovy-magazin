42e8f045653e029a8127ffccccb7152e34b4291d
Iva Havranová <havranova.i@seznam.cz>
2023-07-13 17:31:11 +0200

    other information from database added to the page

--- src/pages/Movie/index.jsx
+++ src/pages/Movie/index.jsx
@@ -9,10 +9,20 @@ export const Movie = () => {
   return (
     <div className="movie">
       <img src={`${moviesData.poster}`} alt={`${moviesData.title}`} />
-
       <h2>{moviesData.title}</h2>
-
       <p>{moviesData.storyline}</p>
+      <p>Year: {moviesData.year}</p>
+      <p>Rating: {moviesData.rating}</p>
+      <p>Director: {moviesData.director}</p>
+      <p>Genre: {moviesData.genre}</p>
+      <h3>Cast</h3>
+      {moviesData.cast.map((actor) => {
+        return (
+          <p>
+            {actor.name} as {actor.as}
+          </p>
+        );
+      })}
     </div>
   );
 };
